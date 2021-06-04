991c67c80bb32f80b6fd93a8b260d73f172b4ac7
prog_zidane <mohamed.zidane95@gmail.com>
2021-06-05 01:10:32 +0200

    update answer.dart button margins

--- lib/Quiz/answer.dart
+++ lib/Quiz/answer.dart
@@ -3,13 +3,14 @@ import 'package:flutter/material.dart';
 class Answer extends StatelessWidget {
   final String answerText;
   final Function onPress;
+
   Answer(this.answerText, this.onPress);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+      margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           primary: Colors.purple,
